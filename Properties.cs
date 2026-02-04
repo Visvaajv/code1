144d354d585f57b0e93a394cf96b4763b957d2ec
VISVAA J <visvaajegadheesvaran@gmail.com>
2026-02-04 17:29:02 +0530

    Add files via upload

---
+++ Properties.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HrLib
+{
+
+    public partial class Empdet
+    {
+        public int Empid { get; set; }
+        public string EmpName { get; set; } = string.Empty;
+        public decimal? Salary { get; set; }
+    }
+
+}
